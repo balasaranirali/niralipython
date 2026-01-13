5a014669b8c95205503ec59ce084d96c74dcb128
balasaranirali <balasaranirali23@gmail.com>
2026-01-13 08:35:54 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,37 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello world");
+hello world
+a=int(input("enter first number"))
+enter first number 2
+b=int(input("enter secound number"))
+enter secound number 2
+sum=a+b
+print("sum=",sum)
+sum= 4
+n=int(input("enter a number:"))
+enter a number:2
+if n%2==0:
+    print("even number")
+else:
+    print("odd number")
+
+    
+even number
+year=int(input("enter a year:"))
+enter a year:2026
+if(year %400==0):
+    print("leap year")
+else:
+    print("not a leap year:")
+
+    
+not a leap year:
+
+import math
+print(math.pi)
+
+x=5
+print(x);
+
+
