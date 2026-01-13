59449112b98a9f39049b516cf731cfa477264cfa
balasaranirali <balasaranirali23@gmail.com>
2026-01-13 08:36:11 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,49 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import math
+print(math.pi)
+3.141592653589793
+a=10
+print(A);
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    print(A);
+NameError: name 'A' is not defined. Did you mean: 'a'?
+A=10
+print(A);
+10
+num=int(input("enter number:"))
+enter number:2
+square=num*num
+print("square=",square)
+square= 4
+import math
+r=float(input("enter radius:"))
+enter radius:3
+area=math.pi*r*r
+print("area of circle=",area)
+area of circle= 28.274333882308138
+a=input("enter anything:")
+enter anything: nirali
+print(type(a))
+<class 'str'>
+import math
+print("square root:",math.sqrt(16))
+square root: 4.0
+print("power:",math.pow(2,3))
+power: 8.0
+print("pi value:",math.pi)
+pi value: 3.141592653589793
+a=int(input("enter basr number:"))
+enter basr number: 2
+b=int(input("enter power:"))
+enter power:2
+print("amswer=",a**b)
+amswer= 4
+n-int(input("enter number:"))
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    n-int(input("enter number:"))
+NameError: name 'n' is not defined
+n=int(input("enter number:"))
+enter number:4
