0b1b4af6df5f4b47b29ee5c675773f962d245289
balasaranirali <balasaranirali23@gmail.com>
2026-01-13 08:36:24 +0530

    Add files via upload

---
+++ assignment3 .py
@@ -0,0 +1,23 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+n=int(input("enter number:"))
+enter number:2
+if n>0:
+    print("positive number")
+elif n<0:
+    print("negative number")
+else:
+    print("zero")
+
+    
+positive number
+
+
+
+
+
+
+
+
+
+
