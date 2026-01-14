0702ee9c2deadd3124ae68db960ae5019f074bc2
balasaranirali <balasaranirali23@gmail.com>
2026-01-14 11:31:52 +0530

    Add files via upload

---
+++ practical task 1.py
@@ -0,0 +1,46 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> b=20
+>>> if a>b:
+...     print("maximum:",a)
+... else:
+...     print("maximum:",b)
+... 
+...     
+maximum: 20
+>>> for i in range(1,5):
+...     print(i)
+... 
+...     
+1
+2
+3
+4
+>>> text="hello"
+>>> print("length:",len(text))
+length: 5
+>>> print("welcome to python programming!")
+welcome to python programming!
+>>> text="python"
+>>> print("first character:",text[0])
+first character: p
+>>> text="python"
+>>> print("last chacter:",text[-1])
+last chacter: n
+>>> num=int(input("enter number:"))
+enter number: 2
+>>> if num>0:
+...     print("positive number")
+... elif num<0:
+...     print("negative number")
+... else:
+...     print("zero")
+... 
+...     
+positive number
+>>> 
+>>> name=input("enter your name:")
+enter your name: nirali balasara
+>>> print("your name is :",name)
+your name is :  nirali balasara
