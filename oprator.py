326a296a52cf776244573535dceb56ac8438c14b
balasaranirali <balasaranirali23@gmail.com>
2026-01-27 08:07:19 +0530

    Add files via upload

---
+++ oprator.py
@@ -0,0 +1,119 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> 
+>>> # assignment oprator
+>>> x=10
+>>> print(x)
+10
+>>> # unary minus
+>>>  a=5
+ 
+SyntaxError: unexpected indent
+>>> a=5
+>>> print(-a)
+-5
+>>> #relational oprators
+>>>  p=10
+ 
+SyntaxError: unexpected indent
+>>> p=10
+>>> q=5
+>>> print(p>q)
+True
+>>> print(p==q)
+False
+>>> #logical oprator
+>>> 
+
+>>> 
+
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#22>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> x
+10
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#24>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> X=true
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    X=true
+NameError: name 'true' is not defined
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> 
+>>> 
+>>> 
+>>> x=True
+>>> y=False
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(nor x)
+SyntaxError: invalid syntax
+>>> print(not x)
+False
+>>> #boolean oprator
+>>> is_pass
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    is_pass
+NameError: name 'is_pass' is not defined
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #bitwais oprator
+>>> a=5
+>>> b=
+SyntaxError: invalid syntax
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a/b)
+1.6666666666666667
+>>> print(x^y)
+True
+>>> x=0
+>>> y=0
+>>> print(x^y)
+0
+>>> x=3
+>>> y=4
+>>> print(x^y)
+7
+>>> print(x&y)
+0
+>>> print(x/y)
+0.75
+>>> #membership oprator
+>>> numbers=[1,2,3,4]
+>>> print(2 in numbers)
+True
+>>> print(5 not in numbers)
+True
+>>> #identity oprator
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
