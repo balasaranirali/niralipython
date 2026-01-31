75058f14cb6a0b4f9cd99b6620efa4459795cc2b
balasaranirali <balasaranirali23@gmail.com>
2026-01-31 09:15:27 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,22 @@
+#print("hello world")
+
+x=20
+#print(x)
+
+#x=20.5
+print(x)
+
+x=("apple","banana","chery")
+#print(x)
+
+#x=True
+print(x)
+
+
+#output statment:
+
+#name=input("enter your name:")
+#print("hello",name)
+
+number=input("enter numbrer:")
+print("hello",number)
