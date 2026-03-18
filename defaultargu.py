ef52ae98907d5d702fede653e2c0acc0873b84df
balasaranirali <balasaranirali23@gmail.com>
2026-03-18 20:51:36 +0530

    Add files via upload

---
+++ defaultargu.py
@@ -0,0 +1,13 @@
+"""def greet(name,msg="good morning"):
+    print("hello",name+"",msg)
+greet("asha")
+greet("ravi","good morning") """
+
+"""def student_info(name,age=18,course="BCA"):
+    print("name:",name)
+    print("age:",age)
+    print("course:",course)
+student_info("ravi")
+student_info("seema",20)
+student_info("amit",19,"Bsc IT")"""
+
