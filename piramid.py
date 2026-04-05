29f3ae4cd68fd3ea145bafa1ccee07181889f6e1
balasaranirali <balasaranirali23@gmail.com>
2026-04-05 15:48:21 +0530

    Add files via upload

---
+++ piramid.py
@@ -0,0 +1,61 @@
+"""n=int(input("enter number of lines:"))
+i=1
+while i<=n:
+    print("*"*i)
+    i+=1"""
+
+
+
+
+
+"""n=int(input("enter number of lines:"))
+i=n
+while i>=1:
+    print("*"* i)
+    i-=1"""
+
+
+
+
+
+
+"""for i in range(1,4):
+    print("*" * i)"""
+
+
+
+
+
+
+
+
+"""n=5
+for i in range(1,n+1):
+    for i in range(1,i+1):
+        print(i,end="")
+        print()"""
+
+
+
+
+
+
+
+
+
+
+"""n=int(input("enter number of lines:"))
+for i in range(1,n+1):
+    print("*" * i)"""
+
+
+
+
+
+
+
+
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is:",total)
